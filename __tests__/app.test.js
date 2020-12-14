46cf14ec6ad351d57e9be3a049cb21b986753533
ErikGraciosa <erikgraciosa@gmail.com>
2020-12-14 14:29:19 -0800

    passing createResponse test and parseRequest test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
-
+    it('the classic true = true', () => {
+        expect(true).toEqual(true)
+    })
 });
