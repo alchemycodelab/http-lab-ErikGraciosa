5c3e08bb8042bb6d595e4d5859a2a1dfc731a796
ErikGraciosa <erikgraciosa@gmail.com>
2020-12-14 16:24:48 -0800

    added green and blue test, red test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,8 +1,31 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('app routes', () => {
-    it('the classic true = true', () => {
-        expect(true).toEqual(true)
-    })
+
+
+describe('server routes', () => {
+    it('GET at "/" returns "hi" ', async() => {
+        const response = await request(app)
+            .get('/');
+        expect(response.text).toEqual('hi');
+    });
+
+    it('confirm GET at "/red" returns body of repsonse ', async() => {
+        const response = await request(app)
+            .get('/red');
+        expect(response.text).toEqual('<h1>red<h1>');
+    });
+
+    it('confirm GET at "/green" returns body of repsonse ', async() => {
+        const response = await request(app)
+            .get('/green');
+        expect(response.text).toEqual('<h1>green<h1>');
+    });
+
+    it('confirm GET at "/blue" returns body of repsonse ', async() => {
+        const response = await request(app)
+            .get('/blue');
+        expect(response.text).toEqual('<h1>blue<h1>');
+    });
+
 });
